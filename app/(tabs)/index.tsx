1cac7ef01aaa7032eaf17ff2bf91e688e2171391
Ivan Tolmachev <ivatolm@gmail.com>
2026-02-05 19:30:13 +0300

    Add home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,116 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Text } from '@rneui/themed';
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+function getPrettyTimeFromUTCStr(utcStr) {
+  const datetime = new Date(utcStr)
+  return datetime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" })
+}
 
-export default function HomeScreen() {
+function getPrettyDateFromUTCStr(utcStr) {
+  const datetime = new Date(utcStr)
+  return datetime.toLocaleDateString("en-US", { day: "numeric", month: "short" })
+}
+
+export default function Tab() {
+  const [games, setGames] = useState<any>([])
+
+  useEffect(() => {
+    const fetchCall = async () => {
+      const result = await fetch("https://api.sstats.net/Games/list?From=2026-02-05&To=2026-02-07&Limit=15", {
+        headers: {
+          'Authorization': 'ApiKey ykl77arevkluz8hx',
+          'Content-Type': 'application/json'
+        }
+      })
+      const games = await result.json()
+      const formattedGames = []
+      for (const game of games.data) {
+        formattedGames.push({
+          date: game.date,
+          countryName: game.season.league.country.name,
+          roundName: game.season.league.name,
+          homeTeam: game.homeTeam.name,
+          awayTeam: game.awayTeam.name
+        })
+        console.log(game)
+      }
+      setGames(formattedGames)
+    }
+
+    console.log("Fetching data from the API...")
+    fetchCall()
+  }, [])
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View style={styles.container}>
+      <Text h4>Popular upcoming matches</Text>
+      <ScrollView>
+      {
+        games.map((game, index) => {
+          return (
+            <View style={styles.cardContainer}>
+              <View style={styles.cardHeaderContainer}>
+                <Text>{game.countryName}</Text>
+                <View style={styles.expander} />
+                <Text>{getPrettyDateFromUTCStr(game.date)}</Text>
+              </View>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+              <View style={styles.cardBodyContainer}>
+                <Text>{game.homeTeam}</Text>
+                <View style={styles.cardBodyTimeContainer}>
+                  <Text>{getPrettyTimeFromUTCStr(game.date)}</Text>
+                </View>
+                <Text>{game.awayTeam}</Text>
+              </View>
+
+              <View style={styles.cardFooterContainer}>
+                <Text>{game.roundName}</Text>
+              </View>
+            </View>
+          )
+        })
+      }
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 5
+  },
+  expander: {
+    flex: 1
+  },
+  cardContainer: {
+    flex: 1,
+    borderRadius: 2,
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    minHeight: 100
+  },
+  cardHeaderContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  cardBodyContainer: {
+    flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  cardBodyTimeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  cardFooterContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderTopWidth: 0.5,
+    alignItems: 'center'
+  }
 });
